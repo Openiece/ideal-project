2eb147af420785d8493e862f3904814537de599f
Enkhmandakh <Enkhmandakh@Buynkhishigs-iMac.local>
2022-11-03 16:56:03 +0800

    Openiece's project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,213 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="team">
+        team <div className="dot">.</div>
+      </div>
+      <div className="title">Instant collaborations for remote teams</div>
+      <div className="t">
+        All in one for your remote team chats, collaboration and track projects
+      </div>
+      <input id="input" placeholder="Email"></input>
+      <div className="btn">Get early access</div>
+      <div className="Products">Products</div>
+      <div className="Services">Services</div>
+      <div className="Contact">Contact</div>
+      <div className="Log">Log in</div>
+      <div className="btn-2">
+        <div className="aoao">Get Access</div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+      <div className="container2">
+        <div className="title2">Your Hub for teamwork</div>
+        <div className="par">
+          Give everyone you work with—inside and outside your company—a more
+          productive way to stay in sync. Respond faster with emoji, keep
+          conversations focused in channels, and simplify all your communication
+          into one place.
+        </div>
+        <div className="rec">Learn more</div>
+        <div className="meetingimg"></div>
+        <div className="base"></div>
+        <div className="aaaa">3</div>
+        <div className="alarm">
+          <div className="clock">11:00 AM - 11:30 AM</div>
+          <div className="titleofalarm">Website for the Product</div>
+          <div className="fill"></div>
+          <div className="am">AM</div>
+          <div className="es">ES</div>
+          <div className="pin"></div>
+          <div className="attach">10 attachements</div>
+        </div>
+        <div className="cal"></div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+      <div className="container3">
+        <div className="con-image">
+          <div className="alarm2">
+            <div className="titleofalarm">
+              Hey you guys we need to do this and that too ✌️
+            </div>
+            <div className="fill"></div>
+            <div className="am">AM</div>
+            <div className="es">ES</div>
+          </div>
+          <div className="alarm3">
+            <div className="clock">11:00 AM - 11:30 AM</div>
+            <div className="titleofalarm">Website for the Product</div>
+            <div className="fill"></div>
+            <div className="am">AM</div>
+            <div className="es">ES</div>
+            <div className="pin"></div>
+            <div className="attach">10 attachements</div>
+          </div>
+        </div>
+        <div className="titleee">Simple task management</div>
+        <div className="para">
+          Give everyone you work with—inside and outside your company—a more
+          productive way to stay in sync. Respond faster with emoji, keep
+          conversations focused in channels, and simplify all your communication
+          into one place.
+        </div>
+        <div className="recoo">Learn more</div>
+      </div>
+
+
+
+
+
+
+
+
+
+      <div className="container4">
+        <div className="idk">Scheduling that actually works</div>
+        <div className="para2">
+          Give everyone you work with—inside and outside your company—a more
+          productive way to stay in sync. Respond faster with emoji, keep
+          conversations focused in channels, and simplify all your communication
+          into one place.
+        </div>
+        <div className="recooo">Learn more</div>
+        <div className="background2">
+          <div className="date"></div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+      <div className="container5">
+        <div className="bigtitle">What people say about us</div>
+        <div className="box1">
+          <img src={require("./star.jpg")} alt="stars" className="stars1"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars2"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars3"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars4"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars5"></img>
+          <div className="parar">
+            Give everyone you work with—inside and outside your emoji, keep
+            conversations focused in channels, and simplify all your
+            communication into one place.
+          </div>
+          <img src={require("./Ellipse.png")} alt="pfp" className="pfp"></img>
+          <div className="pfn">Amy Klassen</div>
+        </div>
+        <div className="box2">
+          <img src={require("./star.jpg")} alt="stars" className="stars1"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars2"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars3"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars4"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars5"></img>
+          <div className="parar">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </div>
+          <img src={require("./aaaa.png")} alt="pfp" className="pfp"></img>
+          <div className="pfn">Jane Cooper</div>
+        </div>
+        <div className="box3">
+          <img src={require("./star.jpg")} alt="stars" className="stars1"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars2"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars3"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars4"></img>
+          <img src={require("./star.jpg")} alt="stars" className="stars5"></img>
+          <div className="parar">
+            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+            cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+            reprehenderit elit laborum.{" "}
+          </div>
+          <img src={require("./pfp.png")} alt="pfp" className="pfp"></img>
+          <div className="pfn">Eleanor Pena</div>
+        </div>
+        <div className="arr">
+          <img
+            src={require("./arrow.png")}
+            alt="arrow"
+            className="arrow1"
+          ></img>
+          <img
+            src={require("./arrow2.png")}
+            alt="arrow"
+            className="arrow2"
+          ></img>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+      <div className="footer">
+        <div className="team2">team<div className="dot2">.</div></div>
+        <img src={require("./Instagram.png")} alt="stars" className="insta"></img>
+        <div className="ig">Instagram</div>
+        <img src={require("./Facebook.png")} alt="stars" className="fb"></img>
+        <div className="face">Facebook</div>
+        <img src={require("./Twitter.png")} alt="stars" className="twitter"></img>
+        <div className="tw">Twitter</div>
+        <img src={require("./Instagram.png")} alt="stars" className="insta1"></img>
+        <div className="ig2">Instagram</div>
+        <img src={require("./Facebook.png")} alt="stars" className="fb1"></img>
+        <div className="face2">Facebook</div>
+        <img src={require("./Twitter.png")} alt="stars" className="twitter1"></img>
+        <div className="tw2">Twitter</div>
+      </div>
+
+
+
     </div>
   );
 }
